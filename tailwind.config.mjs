2fb04aafbf480c85ed43cb6325b272f22808e7a1
Juan Almeida <juanelojga@gmail.com>
2025-11-24 17:20:29 -0500

    Migrate to Tailwind for global styles and integrate Header component into the homepage
    - Simplified styling with TailwindCSS setup and custom configurations.
    - Replaced `header-example.astro` with integration of the `Header` component in `index.astro`.
    - Updated `Header` component structure for improved flexibility and prop defaults.
    - Enabled i18n support in `Header` using dynamic translations.
    - Updated dependencies for Tailwind integration and removed unused files.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,26 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
         brand: {
-          darkest: '#06020a',
+          darkest: "#06020a",
         },
         neutral: {
-          darkest: '#06020a',
-          darker: '#1e1b22',
-          lightest: '#f5f5f5',
+          darkest: "#06020a",
+          darker: "#1e1b22",
+          lightest: "#f5f5f5",
         },
       },
       fontFamily: {
-        sora: ['Sora', 'sans-serif'],
-        inter: ['Inter', 'sans-serif'],
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        sora: ["Sora", ...defaultTheme.fontFamily.sans],
       },
       fontSize: {
-        '6xl': ['64px', { lineHeight: '1.1', letterSpacing: '-0.64px' }],
-        xl: ['20px', { lineHeight: '1.5' }],
-        lg: ['18px', { lineHeight: '1.5' }],
+        "6xl": ["64px", { lineHeight: "1.1", letterSpacing: "-0.64px" }],
+        xl: ["20px", { lineHeight: "1.5" }],
+        lg: ["18px", { lineHeight: "1.5" }],
       },
     },
   },
